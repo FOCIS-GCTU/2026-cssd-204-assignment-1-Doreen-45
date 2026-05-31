7b15ce69092a4da66716a56cdb6271e5ceba01df
Doreen-45 <doreen_2004@icloud.com>
2026-05-31 08:00:04 +0000

    Add files via upload

---
+++ initial.py
@@ -0,0 +1,40 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: ANIMAH DOREEN ABENA
+# STUDENT ID: 2425403192
+# Email: 2425403192@live.gctu.edu.gh
+# Grader: MR.AUGUSTUS
+#
+# On my honor, ANIMAH DOREEN, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print my initials in large letters
+
+    print()
+    print("...ADA")
+    print()
+
+    print("...AAAAAAAAAAAA...DDDDDDDDDDDD...AAAAAAAAAAAA...**")
+    print("...AAAA....AAAA...DDDD....DDDD...AAAA....AAAA...**")
+    print("...AAAA....AAAA...DDDD....DDDD...AAAA....AAAA...**")
+    print("...AAAA....AAAA...DDDD....DDDD...AAAA....AAAA...**")
+    print("...AAAAAAAAAAAA...DDDD....DDDD...AAAAAAAAAAAA...**")
+    print("...AAAAAAAAAAAA...DDDD....DDDD...AAAAAAAAAAAA...**")
+    print("...AAAA....AAAA...DDDD....DDDD...AAAA....AAAA...**")
+    print("...AAAA....AAAA...DDDD....DDDD...AAAA....AAAA...**")
+    print("...AAAA....AAAA...DDDD....DDDD...AAAA....AAAA...**")
+    print("...AAAA....AAAA...DDDDDDDDDDDD...AAAA....AAAA...**")
+    print()
+
+
+main()
+
+
+
+
+
+
